872ebc77a590b802cda8ab5e96bd375b28a37ce9
nathan-77 <47172100+n-athan-77@users.noreply.github.com>
2019-10-02 16:10:30 +0530

    added account

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Branch
+from .models import Branch,Account
 
 
 admin.site.register(Branch)
+admin.site.register(Account)
--- myapp/models.py
+++ myapp/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from random import randint
+from random import randint
 
 
 class Branch(models.Model):
@@ -10,4 +11,25 @@ class Branch(models.Model):
     longitude = models.CharField(max_length=10)
 
     def __str__(self):
-    	return self.branch_name
+        return self.branch_name
+
+
+class Account(models.Model):
+    acc_no = models.AutoField(primary_key=True)
+    branch_id = models.ForeignKey(Branch, on_delete=models.CASCADE)
+    acc_type = models.CharField(max_length=50)
+    acc_balance = models.BigIntegerField()
+
+    def __str__(self):
+        return str(self.acc_no)
+
+
+
+    def save(self):
+        if not self.acc_no:
+            is_unique = False
+            while not is_unique:
+                acc_no = randint(10000000000, 99999999999) # 19 digits: 1, random 18 digits
+                is_unique = (Account.objects.filter(acc_no=acc_no).count() == 0)
+            self.acc_no = acc_no
+        super(Account, self).save()
